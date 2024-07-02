2f22d380e03883c9cd91a001f1d2ed93be69cdc5
Rajeev Ranjan <rjranjan2112@gmail.com>
2024-07-02 20:35:28 +0530

    First Commit

---
+++ Main.java
@@ -0,0 +1,26 @@
+public class Main {
+    public static void main(String[] args){
+
+        
+        int rollno = 44;
+        System.out.println(rollno);
+        
+        String name = "Rajeev";
+        System.out.println(name);
+        
+        char letter = 'r';
+        System.out.println(letter);
+        
+        float marks = 98.67f;
+        System.out.println(marks);
+        
+        double large = 73898230.329393;
+        System.out.println(large);
+        
+        long largeInt = 212323728;
+        System.out.println(largeInt);
+        
+        boolean check = false;
+        System.out.println(check);
+    }
+}
